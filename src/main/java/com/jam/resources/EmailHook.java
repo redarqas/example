187490dca51e23a54e30cd2273b1f1183abc821c
Jamal CHAQOURI <jamal.chaqouri@gmail.com>
2016-02-05 01:29:23 +0100

    uu

--- src/main/java/com/jam/resources/EmailHook.java
+++ src/main/java/com/jam/resources/EmailHook.java
@@ -4,6 +4,7 @@ import com.jam.core.EmailMessage;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.function.Function;
@@ -16,8 +17,9 @@ public abstract class EmailHook<T> {
         this.transform = transform;
     }
 
+    @Path("/callback")
     @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Consumes(MediaType.WILDCARD)
     public Response callback(T msg) {
         System.out.println("Receive Message");
         EmailMessage email = transform.apply(msg);
